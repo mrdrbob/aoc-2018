5c5bbc8eebb3680a0741768a8c3338408d51460f
Bob Davidson <bob@blendinteractive.com>
2018-12-09 14:24:08 -0600

    Day 7 step 2

--- src/day7.rs
+++ src/day7.rs
@@ -22,8 +22,15 @@ pub fn day_seven_go_do_thing() {
     }
 
     let mut met_dependencies:HashSet<char> = HashSet::new();
-    let mut max_cycles = 5000;
+    let mut max_cycles = 50000;
+    let mut seconds_spent = 0;
+    let total_workers = 5;
+
+    // Represents the amount of time remaining for a given job.
+    let mut workers_currently_working:Vec<Work> = Vec::new();
+
     while dependency_map.len() > 0 && max_cycles > 0 {
+        // Calculate which jobs are available based on met dependencies
         let mut ready_to_run:Vec<char> = dependency_map.iter()
             .filter(|(_, v)|  {
                 v.iter().filter(|x| !met_dependencies.contains(&x)).count() == 0
@@ -31,30 +38,68 @@ pub fn day_seven_go_do_thing() {
             .map(|(k, _)| k.clone())
             .collect();
         
-        if ready_to_run.len() == 0 {
-            panic!();
-        }
-
+        // Sort the list
         ready_to_run.sort();
 
-        // for c in ready_to_run {
-        let c = ready_to_run.first().unwrap();
-        print!("{}", c);
-        met_dependencies.insert(c.clone());
+        // Figure out how many (if any) workers are available to take on jobs.
+        let available_workers = total_workers - workers_currently_working.len();
+        let jobs_to_assign = available_workers.min(ready_to_run.len());
 
-        met_dependencies.insert(c.clone());
-        dependency_map.remove(c);
+        // println!("{:?}", workers_currently_working);
+        // println!("{} {} {}", ready_to_run.len(), available_workers, jobs_to_assign );
 
-    
+        // Dole out those jobs
+        for _ in 0..jobs_to_assign {
+            let letter = ready_to_run.first().expect("A job").clone();
+            let letter_length = letter.to_string().bytes().last().unwrap() - 64;
+            let job_length:i32 = 60 + i32::from(letter_length);
+            
+            // println!("{}", job_length);
+            workers_currently_working.push(Work {
+                id: letter.clone(),
+                time_remaining: job_length
+            });
+
+            // Remove it from the read-to-run list
+            ready_to_run.remove(0);
+            dependency_map.remove(&letter);
+        }
+
+        // println!("{:?}", workers_currently_working);
+
+        // Spend time on each of the items, remove worker if they only have 1 second remaining
+        let mut new_time:Vec<Work> = Vec::new();
+        while workers_currently_working.len() > 0 {
+            let current_value = workers_currently_working.remove(0);
+            match current_value.time_remaining {
+                1 => {
+                    // This dep is now met
+                    met_dependencies.insert(current_value.id.clone());
+                }
+                v => new_time.push(Work {
+                    id: current_value.id.clone(),
+                    time_remaining: v - 1
+                })
+            }
+        }
+
+        workers_currently_working = new_time;
 
         max_cycles -= 1;
+        seconds_spent += 1;
     }
 
     if max_cycles <= 1 {
         panic!("Infinite loop");
     }
 
+    let remaining = workers_currently_working.iter().max_by_key(|g| g.time_remaining);
+    let seconds_remaining = match remaining {
+        Some(s) => s.time_remaining,
+        None => 0
+    };
 
+    println!("{}", seconds_spent + seconds_remaining);
 }
 
 fn ensure_record(mut map:HashMap<char, HashSet<char>>, step:char) -> HashMap<char, HashSet<char>> {
@@ -73,6 +118,11 @@ fn add_dependency(mut map:HashMap<char, HashSet<char>>, step:char, dep:char) -> 
     vector.insert(dep);
     map.insert(step, vector);
     map
+}
+
+struct Work {
+    id: char,
+    time_remaining: i32
 }
 
 #[derive(Debug)]
